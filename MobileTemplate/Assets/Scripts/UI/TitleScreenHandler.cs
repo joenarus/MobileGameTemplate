328b92e13ffa43cf5673d10973277e8517711e5f
joenarus <joe.narus2@gmail.com>
2018-10-07 12:26:27 -0600

    Title Screen and Scene Loader

---
+++ MobileTemplate/Assets/Scripts/UI/TitleScreenHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScreenHandler : MonoBehaviour {
+
+    private SceneLoader sceneLoader;
+    private Button button;
+
+    public string FirstLevelName;
+	// Use this for initialization
+	void Start () {
+        sceneLoader = (SceneLoader)GameObject.FindGameObjectWithTag("Loader").GetComponent("SceneLoader");
+        button = (Button)gameObject.GetComponent("Button");
+
+        button.onClick.AddListener(() => sceneLoader.LoadScene(FirstLevelName));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+
+    
+}
